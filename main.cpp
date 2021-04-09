23fe66b2c2d97d86ff9edfedcbf7f3ad67f5356d
Cameron <cameronkburley@gmail.com>
2021-04-09 09:36:43 -0700

    Fixed segmentation fault and double free error

--- main.cpp
+++ main.cpp
@@ -3,26 +3,26 @@
 #include <string>
 using namespace std;
 
-void recordInfo(int startIdx, int endIdx, char *info, char *data)
-{
+void recordInfo(int startIdx, int endIdx, char* info, char *data)
+{   
     for (int i = startIdx; i < endIdx; i++)
     {
         info[i - startIdx] = data[i];
     }
-
     info[endIdx] = 0;
 }
 
 int main(int argc, char **argv)
 {
     char line[80];
-    char **addresses = new char *[4];
+    string * addresses;
+    addresses = new string[20];
     ifstream myfile("P2sampleAdder.lis");
 
     if (myfile.is_open())
     {
         int idx = 0;
-        char *address;
+        char* address;
 
         while (myfile.getline(line, 80))
         {
@@ -31,17 +31,17 @@ int main(int argc, char **argv)
             addresses[idx] = address;
             idx++;
         }
-
+        
         for (int i = 0; i < 20; i++)
         {
             cout << addresses[i] << endl;
         }
-
         myfile.close();
-    }
-
-    else
+        delete[] address;
+        delete[] addresses;
+    }else
     {
         cout << "Unable to open file";
     }
+    return 0;
 }
