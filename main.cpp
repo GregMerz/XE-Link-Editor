536f8bec9512c724035f886e69afc0484a5de498
Cameron <cameronkburley@gmail.com>
2021-04-17 00:12:00 -0700

    Don't forget to add flags to symTab

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <map>
 using namespace std;
 
-map<string, string> symTab;
+map<string, string> symTab; //<----------------- Add Flags
 string getInfo(int startIdx, string data){
     string delimiter = " ";
     string info = data.substr(startIdx, data.find(delimiter)+4); //4 for 4 bytes? string or char?
